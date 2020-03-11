8a07f9a5a941e5203f33458d45aa8aa027759ba4
Mina <zetech1010@gmail.com>
2020-03-12 03:38:25 +0800

    TODOListViewController完成DataSource和delegate方法

---
+++ TODO/ToDoListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by sc on 2020/3/12.
+//  Copyright © 2020 Zetech. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["购买水杯","吃药","修改密码"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        //标志的切换
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        //动态取消高亮状态
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+
+
+}
+
