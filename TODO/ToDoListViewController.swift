25e1f451a68581f28ec3298ee36a26fffc23ab46
Mina <zetech1010@gmail.com>
2020-03-13 01:17:45 +0800

    添加项目功能完成

--- TODO/ToDoListViewController.swift
+++ TODO/ToDoListViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["购买水杯","吃药","修改密码"]
+    var itemArray = ["购买水杯","吃药","修改密码"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //添加一个生存期在方法内部的新变量，用来在添加项目时候接收用户输入
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "添加一个新的ToDo项目", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "添加项目", style: .default, handler: {action in
+            //用户单击时候执行的代码
+            print(textField.text!)
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        })
+        alert.addTextField(configurationHandler: {alertTextField in
+            alertTextField.placeholder = "创建一个新项目..."
+            //让textField指向alertTextField
+            textField = alertTextField
+        })
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     //MARK: - TableView DataSource Methods
